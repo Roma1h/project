36869a8617d6d3c11764ca5d705b2bee5b0507f0
Roman <example@gmail.com>
2023-08-12 10:30:39 -0600

    firstPrCommit

--- js/script.js
+++ js/script.js
@@ -1,26 +1,31 @@
 "use strict";
 
-console.log('arr' + "- object");
-console.log(4 + +"5");
+//1)
+const numberofFilms=+prompt('Сколько фильмов вы уже посмотрели?',' ' );
+console.log(numberofFilms);
 
-let incr=10,
-    decr=10;
+
+//2)
+
+const personalMovieDB = {
+    count:numberofFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+
+};
+
+//3)
+const a =prompt('Один из последних просмотренных фильмов?',''),
+ b=prompt('На сколько оцените его?',''),
+ c=prompt('Один из последних просмотренных фильмов?',''),
+ d=prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+console.log(personalMovieDB);
+
     
-    //++incr;
-    //--decr;
-
-console.log(++incr);
-console.log(--decr);
-
-console.log(5%2);
-
-console.log(2+2*2 != '6');
-
-const isChecked=false,
-isClose=false;
-
-console.log(isChecked && isClose );
-
-console.log(isChecked || !isClose);
 
 
